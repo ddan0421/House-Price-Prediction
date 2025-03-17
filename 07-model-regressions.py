9391e4bd9c49d549b63d6876f19097c2380bb414
Dan Dan <ddan@pnmac.com>
2025-03-17 11:58:48 -0700

    Add regressions and eval codes

--- 07-model-regressions.py
+++ 07-model-regressions.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import models
+import duckdb
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score
+
 
 
 X_train = pd.read_csv("data/model_data/X_train.csv")
@@ -14,10 +20,7 @@ y_val = pd.read_csv("data/model_data/y_val.csv")
 X_train = sm.add_constant(X_train)
 X_val = sm.add_constant(X_val)
 
-
-
-
-############################## VIF ##############################
+############################## Feature Selection 1: VIF ##############################
 vif_data = pd.DataFrame()
 vif_data["feature"] = X_train.columns
 vif_data["VIF"] = [variance_inflation_factor(X_train.values, i) for i in range(X_train.shape[1])]
@@ -27,11 +30,7 @@ vif_data = vif_data[vif_data["VIF"] != np.inf]
 vif_data.sort_values(by="VIF", ascending=True).to_csv("vif_data.csv", index=False)
 selected_features_vif = list(vif_data[vif_data["VIF"] < 10]["feature"])
 
-############################# Random Forest Feature Importance #############################
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-
-# Load data
+############################# Feature Selection 2: Random Forest Feature Importance #############################
 X_train = pd.read_csv("data/model_data/X_train.csv")
 X_val = pd.read_csv("data/model_data/X_val.csv")
 y_train = pd.read_csv("data/model_data/y_train.csv")
@@ -47,28 +46,48 @@ feature_importance = pd.DataFrame({
     "importance": rf_model.feature_importances_
 })
 
-# Sort feature importance in descending order
-sorted_indices = np.argsort(feature_importance['importance'])[::-1]
-importances = feature_importance['importance'].values[sorted_indices]
-
-# Calculate cumulative importance
-cumulative_importance = np.cumsum(importances)
-
-# Define the threshold for cumulative importance (e.g., 90% or 95%)
+conn = duckdb.connect()
+# Define the threshold for cumulative importance
 threshold = 0.95
 
-# Find the index where the cumulative importance reaches or exceeds the threshold
-threshold_index = np.where(cumulative_importance >= threshold)[0][0]
+# Calculate cumulative importance and filter features
+# Keep enough features such that their cumulative importance adds up to 95% of the total importance.
+query = f"""
+WITH sorted_importance AS (
+    SELECT
+        feature,
+        importance
+    FROM feature_importance
+    ORDER BY importance DESC
+),
+cumulative_importance AS (
+    SELECT
+        feature,
+        importance,
+        SUM(importance) OVER (ORDER BY importance DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_importance,
+        ROW_NUMBER() OVER (ORDER BY importance DESC) AS row_number
+    FROM sorted_importance
+),
+threshold_index AS (
+    SELECT
+        MIN(row_number) AS threshold_row
+    FROM cumulative_importance
+    WHERE cumulative_importance >= {threshold}
+)
+SELECT
+    feature
+FROM cumulative_importance
+INNER JOIN threshold_index
+ON cumulative_importance.row_number <= threshold_index.threshold_row;
 
-# Select features that explain up to the threshold cumulative importance
-selected_features_rf = feature_importance['feature'].values[sorted_indices][:threshold_index + 1]
+"""
 
-# Print selected features
-print(f"Features selected based on cumulative importance ({threshold*100}%):")
-print(selected_features_rf)
+# Execute the query to get selected features
+selected_features_rf = conn.execute(query).fetch_df()["feature"].to_list()
+conn.close()
 
 
-############################# Selected Numeric #############################
+############################# Feature Selection 3: Selected Numeric Variables based on Correlation Matrix and Domain Knowledge #############################
 selected_numeric_features = [
     "log_LotArea", "cbrt_MasVnrArea", "sqrt_TotalBsmtSF", "log_1stFlrSF", 
     "log_GrLivArea", "BsmtFullBath", "FullBath", "HalfBath", "BedroomAbvGr", 
@@ -78,10 +97,116 @@ selected_numeric_features = [
 
 
 # Combine the three lists and remove duplicates using a set
-combined_features = list(set(selected_features_vif + selected_features_rf.tolist() + selected_numeric_features))
+combined_features = list(set(selected_features_vif + selected_features_rf + selected_numeric_features))
 combined_features.sort()
 
 
+
+############################# Linear Regression #############################
 X_train_new = sm.add_constant(X_train[combined_features])
+X_val_new = sm.add_constant(X_val[combined_features])
+
 ols_lr = models.sm_ols(X_train_new, y_train)
 glm_lr = models.sm_glm_gaussian(X_train_new, y_train)
+glm_lr_constrained = models.constrained_sm_glm_gaussian(X_train_new, y_train, glm_lr, 0.05)
+
+############################# Regularized Regression Models #############################
+ml_features = glm_lr_constrained.params.index[(glm_lr_constrained.params != 0) & (glm_lr_constrained.params.index != "const")].to_list()
+X_train_ml = X_train[ml_features]
+X_val_ml = X_val[ml_features]
+
+############################# Ridge Regression #############################
+# Nested Cross-Validation
+random_state = 42
+inner_cv = KFold(n_splits=10, shuffle=True, random_state=random_state)
+outer_cv = KFold(n_splits=10, shuffle=True, random_state=random_state)
+
+# Define the Ridge regression model
+ridge = Ridge()
+
+# Define a grid of hyperparameters to search
+param_grid = {
+    "alpha": [0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]  # Values of alpha to search
+}
+
+# Hyperparameter tuning for Ridge Regression
+gs_ridge = GridSearchCV(estimator=ridge,
+                        param_grid=param_grid,
+                        scoring="neg_root_mean_squared_error",  # Using RMSE as the evaluation metric
+                        cv=inner_cv,
+                        n_jobs=-1)
+
+gs_ridge.fit(X_train_ml, y_train)
+
+print("Non-nested CV RMSE (log-transformed scale)::", -gs_ridge.best_score_)  # RMSE is the negative value from GridSearchCV
+print("Optimal Parameter:", gs_ridge.best_params_)
+print("Optimal Estimator:", gs_ridge.best_estimator_)
+
+nested_score_gs_ridge_rmse = cross_val_score(gs_ridge, X=X_train_ml, y=y_train, scoring="neg_root_mean_squared_error", cv=outer_cv)
+print("Nested CV RMSE (log-transformed scale):", -nested_score_gs_ridge_rmse.mean(), " +/- ", nested_score_gs_ridge_rmse.std())  # Multiply by -1 to get positive RMSE
+
+# Fit the final model on the entire training data
+final_model_ridge = gs_ridge.best_estimator_.fit(X_train_ml, y_train)
+
+# Extract the selected features based on non-zero coefficients from Ridge regression
+selected_features_ridge = X_train_ml.columns[final_model_ridge.coef_.flatten() != 0]
+
+print("Selected features for Ridge:")
+print(selected_features_ridge)
+
+############################# Lasso Regression #############################
+# Nested Cross-Validation
+random_state = 42
+inner_cv = KFold(n_splits=10, shuffle=True, random_state=random_state)
+outer_cv = KFold(n_splits=10, shuffle=True, random_state=random_state)
+
+# Define the Lasso regression model
+lasso = Lasso()
+
+# Define a grid of hyperparameters to search
+param_grid = {
+    "alpha": [0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]  # Values of alpha to search
+}
+
+# Hyperparameter tuning for Lasso Regression
+gs_lasso = GridSearchCV(estimator=lasso,
+                        param_grid=param_grid,
+                        scoring="neg_root_mean_squared_error",  # Using RMSE as the evaluation metric
+                        cv=inner_cv,
+                        n_jobs=-1)
+
+gs_lasso.fit(X_train_ml, y_train)
+
+print("Non-nested CV RMSE:", -gs_lasso.best_score_)  # RMSE is the negative value from GridSearchCV
+print("Optimal Parameter:", gs_lasso.best_params_)
+print("Optimal Estimator:", gs_lasso.best_estimator_)
+
+nested_score_gs_lasso_rmse = cross_val_score(gs_lasso, X=X_train_ml, y=y_train, scoring="neg_root_mean_squared_error", cv=outer_cv)
+print("Nested CV RMSE:", -nested_score_gs_lasso_rmse.mean(), " +/- ", nested_score_gs_lasso_rmse.std())  # Multiply by -1 to get positive RMSE
+
+# Fit the final model on the entire training data
+final_model_lasso = gs_lasso.best_estimator_.fit(X_train_ml, y_train)
+
+# Extract the selected features based on non-zero coefficients from Lasso regression
+selected_features_lasso = X_train_ml.columns[final_model_lasso.coef_.flatten() != 0]
+
+print("Selected features for Lasso:")
+print(selected_features_lasso)
+
+############################################## Models Generalization Performance ##############################################
+def evaluate_model(model, X, y, name):
+    predictions = model.predict(X)
+    predictions = np.expm1(predictions)
+    y_actual = np.expm1(y)
+    rmse = root_mean_squared_error(y_actual, predictions)
+    print(f"{name} Performance:")
+    print(f"Root Mean Squared Error: {rmse:.4f}")
+
+evaluate_model(ols_lr, X_val_new, y_val, "OLS Model")
+evaluate_model(glm_lr, X_val_new, y_val, "GLM Gaussian Model")
+evaluate_model(glm_lr_constrained, X_val_new, y_val, "Constrained GLM Gaussian Model")
+evaluate_model(final_model_ridge, X_val_ml, y_val, "Ridge Model")
+evaluate_model(final_model_lasso, X_val_ml, y_val, "Lasso Model")
+
+
+
