d8184c7dcd477b9a3f4befe441bf37df1310a663
Dan Dan <ddan@pnmac.com>
2025-05-02 20:56:59 -0700

    Add installing dependencies script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+pip install --upgrade pip
+pip install statsmodels seaborn matplotlib scipy scikit-learn xgboost lightgbm==4.5.0 bayesian-optimization
+echo "All dependencies have been installed successfully!"
