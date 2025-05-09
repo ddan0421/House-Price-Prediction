5c76a5ce2d678a94e6915e151d4f4c718369825e
Dan Dan <ddan@pnmac.com>
2025-05-09 10:00:08 -0700

    Add dependencies versions

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pip install --upgrade pip
-pip install statsmodels seaborn matplotlib scipy scikit-learn xgboost lightgbm==4.5.0 bayesian-optimization
+pip install statsmodels==0.14.2 seaborn matplotlib scipy scikit-learn==1.5.1 xgboost==2.1.1 lightgbm==4.5.0 bayesian-optimization==2.0.3 optuna==4.3.0 catboost==1.2.8
 echo "All dependencies have been installed successfully!"
