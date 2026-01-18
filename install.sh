1d515f0a4a4bb5b89dd90e424f6e8a5ec6a03fed
Dan Dan <ddanzk28@gmail.com>
2026-01-17 21:21:41 -0800

    implement uv for python package management

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pip install --upgrade pip
-pip install statsmodels==0.14.2 seaborn matplotlib scipy scikit-learn==1.5.1 xgboost==2.1.1 lightgbm==4.5.0 bayesian-optimization==2.0.3 optuna==4.3.0 catboost==1.2.8 dask[dataframe]
-echo "All dependencies have been installed successfully!"
