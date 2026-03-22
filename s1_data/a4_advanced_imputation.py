58512d5d1894337c83481f37c2f61438f14b33d1
Dan Dan <ddanzk28@gmail.com>
2026-03-21 19:02:38 -0700

    s1_data/a4_advanced_imputation.py: use the old version of creating categorical and numerical interactions and imputations

--- s1_data/a4_advanced_imputation.py
+++ s1_data/a4_advanced_imputation.py
@@ -63,39 +63,48 @@ test["Functional"].fillna(X_train["Functional"].mode()[0], inplace=True)
 """
 categorical:
 - combine MSSubClass and MSZoning
-- combine Condition1 and Condition2
+- combine LotConfig and LandSlope
+- combine Neighborhood and Condition1 and Condition2
 - combine BldgType and HouseStyle
 - combine Exterior1st and Exterior2nd
+- combine CentralAir and Electrical
+- combine LotShape and LandContour
+- combine RoofStyle and RoofMatl
+- combine Heating and HeatingQC  
 - capture the seasonality of the sale based on MoSold
 
 Feature engineering with year and month variables
 - YearBuilt
 - YearRemodAdd
-
+- GarageYrBlt
+- YrSold
 
 """
 
 
 def feature_engineering(df):
-    conn = duckdb.connect(database=":memory:")
+    conn = duckdb.connect()
     conn.register("original_df", df)
     query = """
     WITH cte AS (
     SELECT 
         *,
         CAST(MSSubClass AS TEXT) || '_' || MSZoning AS MSSubClass_MSZoning,
-        CASE 
-        WHEN Condition1 IN ('PosN','PosA') 
-            OR Condition2 IN ('PosN','PosA') THEN 'Positive'
-        WHEN Condition1 IN ('Artery','Feedr','RRAn','RRNn','RRAe','RRNe') 
-            OR Condition2 IN ('Artery','Feedr','RRAn','RRNn','RRAe','RRNe') THEN 'Negative'
-        ELSE 'Normal'
-        END AS Condition_Class,
+        LotConfig || '_' || LandSlope AS LotConfig_LandSlope,
+        Neighborhood || '_' || 
+            CASE 
+                WHEN Condition1 = Condition2 THEN Condition1 
+                ELSE Condition1 || '_' || Condition2 
+            END AS Neighborhood_Condition,
         BldgType || '_' || HouseStyle AS BldgType_HouseStyle,
         CASE 
             WHEN Exterior1st = Exterior2nd THEN Exterior1st 
             ELSE Exterior1st || '_' || Exterior2nd 
         END AS Exterior1st_Exterior2nd,
+        CentralAir || '_' || Electrical AS CentralAir_Electrical,
+        LotShape || '_' || LandContour AS LotShape_LandContour,
+        RoofStyle || '_' || RoofMatl AS RoofStyle_RoofMatl,
+        Heating || '_' || HeatingQC AS Heating_HeatingQC,
         CASE
            WHEN MoSold IN (12, 1, 2) THEN 'Winter'
            WHEN MoSold IN (3, 4, 5) THEN 'Spring'
@@ -103,37 +112,40 @@ def feature_engineering(df):
            ELSE 'Fall'
         END AS Season_Sold,
         IF((YrSold - YearBuilt) < 0 OR YrSold IS NULL OR YearBuilt IS NULL, 0, (YrSold - YearBuilt)) AS Age_House,
-        IF((YrSold - YearRemodAdd) < 0 OR YrSold IS NULL OR YearRemodAdd IS NULL, 0, (YrSold - YearRemodAdd)) AS Yrs_Since_Remodel
+        IF((YrSold - YearRemodAdd) < 0 OR YrSold IS NULL OR YearRemodAdd IS NULL, 0, (YrSold - YearRemodAdd)) AS Yrs_Since_Remodel,
+        IF((YrSold - GarageYrBlt) < 0 OR GarageYrBlt IS NULL OR GarageType = 'no_garage', 0, (YrSold - GarageYrBlt)) AS Age_Garage
     FROM original_df)
     
-    SELECT * EXCLUDE ("MSSubClass", "MSZoning", 
-        "Condition1", "Condition2",
+    SELECT * EXCLUDE ("MSSubClass", "MSZoning", "LotConfig", "LandSlope", 
+        "Condition1", "Condition2", "Neighborhood", 
         "BldgType", "HouseStyle", 
         "Exterior1st", "Exterior2nd", 
-        "YearBuilt", "YearRemodAdd")
+        "CentralAir", "Electrical", 
+        "LotShape", "LandContour", 
+        "RoofStyle", "RoofMatl", 
+        "Heating", "HeatingQC", "MoSold", "YearBuilt", "YearRemodAdd", "GarageYrBlt", "YrSold")
     FROM cte;
     """
     result = conn.query(query).fetchdf()
     conn.close()
     return result
 
-
 X_train_engineered = feature_engineering(X_train)
 X_val_engineered = feature_engineering(X_val)
 test_engineered = feature_engineering(test)
 
 # Step 4: Encode nominal categorical variables separately for train, validation, and test sets to prevent data leakage, ensuring consistent feature alignment using one-hot encoding
-nominal_cat = ["MSSubClass_MSZoning", "LotConfig", "Neighborhood", "Condition_Class", "BldgType_HouseStyle",
-               "Exterior1st_Exterior2nd", "Electrical", "LandContour", "RoofStyle", "RoofMatl",
-               "Heating", "Alley", "MasVnrType", "Foundation", 
+nominal_cat = ["MSSubClass_MSZoning", "LotConfig_LandSlope", "Neighborhood_Condition", "BldgType_HouseStyle",
+               "Exterior1st_Exterior2nd", "CentralAir_Electrical", "LotShape_LandContour", "RoofStyle_RoofMatl",
+               "Heating_HeatingQC", "Alley", "MasVnrType", "Foundation", 
                "GarageType", "PavedDrive", 
                "Fence", "MiscFeature", "SaleType", "SaleCondition", "Season_Sold"]
 
-ordinal_cat = ["LandSlope", "LotShape", "HeatingQC", "Utilities", "Functional", "OverallQual", "OverallCond", "ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "BsmtExposure", 
+ordinal_cat = ["Utilities", "Functional", "OverallQual", "OverallCond", "ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "BsmtExposure", 
                "BsmtFinType1", "BsmtFinType2", "KitchenQual", "FireplaceQu", "GarageFinish", "GarageQual", "GarageCond", 
                "PoolQC"]
 
-binary_nominal = ["CentralAir", "Street"]
+binary_nominal = ["Street"]
 
 # One-hot encode nominal categorical variables
 X_train_encoded = pd.get_dummies(X_train_engineered, columns=nominal_cat, drop_first=True)
@@ -151,27 +163,6 @@ def ordinal_encoding(df):
     WITH cte AS (
     SELECT 
         *,
-        CASE
-            WHEN LandSlope = 'Gtl' THEN 3
-            WHEN LandSlope = 'Mod' THEN 2
-            WHEN LandSlope = 'Sev' THEN 1
-            ELSE 0
-        END AS LandSlope_encoded,
-        CASE
-            WHEN LotShape = 'Reg' THEN 4
-            WHEN LotShape = 'IR1' THEN 3
-            WHEN LotShape = 'IR2' THEN 2
-            WHEN LotShape = 'IR3' THEN 1
-            ELSE 0
-        END AS LotShape_encoded,
-        CASE
-            WHEN HeatingQC = 'Ex' THEN 5
-            WHEN HeatingQC = 'Gd' THEN 4
-            WHEN HeatingQC = 'TA' THEN 3
-            WHEN HeatingQC = 'Fa' THEN 2
-            WHEN HeatingQC = 'Po' THEN 1
-            ELSE 0
-        END AS HeatingQC_encoded,
         CASE
             WHEN Utilities = 'AllPub' THEN 4
             WHEN Utilities = 'NoSewr' THEN 3
@@ -295,11 +286,6 @@ def ordinal_encoding(df):
             ELSE 0
         END AS PoolQC_encoded,
         CASE
-            WHEN CentralAir = 'N' THEN 0
-            WHEN CentralAir = 'Y' THEN 1
-            ELSE 0
-        END AS CentralAir_encoded,
-        CASE
             WHEN Street = 'Grvl' THEN 0
             WHEN Street = 'Pave' THEN 1
             ELSE 0
@@ -307,7 +293,7 @@ def ordinal_encoding(df):
     FROM input_df)
     
     SELECT * EXCLUDE (
-        "LandSlope", "LotShape", "HeatingQC", "Utilities", "Functional", "CentralAir", "Street",
+        "Utilities", "Functional", "Street",
         "ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "BsmtExposure", 
         "BsmtFinType1", "BsmtFinType2", "KitchenQual", "FireplaceQu", "GarageFinish", "GarageQual", "GarageCond", 
         "PoolQC")
@@ -333,11 +319,9 @@ test_encoded[bool_columns_test] = test_encoded[bool_columns_test].astype("int8")
 ############################### Train Data LotFrontage Missing Data Imputation ########################################
 # Step 6: Impute missing continuous numerical data in the training set using IterativeImputer with BayesianRidge estimator
 # Columns to be used as predictors for imputing "LotFrontage"
-columns_for_imputation = [
-    "LotArea", "1stFlrSF", "Street_encoded", "LotShape_encoded"
-] + [col for col in X_train_encoded.columns if col.startswith("Neighborhood_")] \
-  + [col for col in X_train_encoded.columns if col.startswith("LandContour_")] \
-  + [col for col in X_train_encoded.columns if col.startswith("LotConfig_")] \
+columns_for_imputation = ["LotArea", "1stFlrSF", "Street_encoded"] \
+  + [col for col in X_train_encoded.columns if col.startswith("Neighborhood_Condition_")] \
+  + [col for col in X_train_encoded.columns if col.startswith("LotShape_LandContour_")] \
   + [col for col in X_train_encoded.columns if col.startswith("BldgType_HouseStyle_")]
 
 iterative_imputer = IterativeImputer(estimator=BayesianRidge(), random_state=42)
