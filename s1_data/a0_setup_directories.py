7e8e26ec7406cadc5d33445a98e4fc01bbd75309
Dan Dan <ddanzk28@gmail.com>
2026-04-11 13:08:57 -0700

    s1_data/a0_setup_directories.py: modify directories to create only data and models folders

--- s1_data/a0_setup_directories.py
+++ s1_data/a0_setup_directories.py
@@ -1,7 +1,7 @@
 import os
 
-base_folder = "data"
-os.makedirs(base_folder, exist_ok=True)
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
 
-model_data_dir = "model_data"
-os.makedirs(os.path.join(base_folder, model_data_dir), exist_ok=True)
+model_dir = "models"
+os.makedirs(model_dir, exist_ok=True)
