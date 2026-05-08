7abac88caa44699bdc4da074c89b4d378ecf855d
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-05-08 15:45:34 -0700

    s2_model/a4_trees.py: add rf and extra tree

--- s2_model/a4_trees.py
+++ s2_model/a4_trees.py
@@ -70,4 +70,76 @@ print("decision tree model saved to models/final_model_dt.pkl")
 evaluate_model(final_model_dt, X_val_tree_raw, y_val, "Decision Tree")
 
 
+############################################## Random Forest Tree Regressor Model ############################################################
+rf = RandomForestRegressor(random_state=random_state, bootstrap=True)
+
+param_grid = {
+    "n_estimators": [50, 100, 200], 
+    "max_depth": [3, 5, 10], 
+    "min_samples_split": [2, 5],  
+    "min_samples_leaf": [1, 2], 
+    "max_features": ["sqrt", "log2"],  
+}
+
+gs_rf = GridSearchCV(estimator=rf,
+                     param_grid=param_grid,
+                     scoring="neg_root_mean_squared_error", 
+                     cv=cv,
+                     n_jobs=-1,
+                     refit=True)
+
+gs_rf.fit(X_train_tree_raw, y_train.values.ravel())
+
+print("10-Fold CV RMSE:", -gs_rf.best_score_) 
+print("Optimal Parameters:", gs_rf.best_params_)
+print("Optimal Estimator:", gs_rf.best_estimator_)
+
+final_model_rf = gs_rf.best_estimator_
+
+selected_features_rf = X_train_tree_raw.columns[np.array(final_model_rf.feature_importances_) > 0]
+print("Selected features for Random Forest:")
+print(selected_features_rf)
+
+# Save the trained model for future use (stacking)
+with open("models/final_model_rf.pkl", "wb") as f:
+    pickle.dump(final_model_rf, f)
+print("random forest model saved to models/final_model_rf.pkl")
+
+evaluate_model(final_model_rf, X_val_tree_raw, y_val, "Random Forst Tree")
+
+
+############################################## ExtraTreesRegressor Model ############################################################
+et = ExtraTreesRegressor(random_state=random_state, criterion='squared_error')
+
+param_grid = {
+    "n_estimators": [50, 100, 200],  
+    "max_depth": [3, 5, 10],  
+    "min_samples_split": [2, 5],  
+    "min_samples_leaf": [1, 2], 
+    "max_features": ["sqrt", "log2", None], 
+    "bootstrap": [True, False]  
+}
+
+gs_et = GridSearchCV(estimator=et, 
+                     param_grid=param_grid, 
+                     scoring="neg_root_mean_squared_error", 
+                     cv=cv, 
+                     n_jobs=-1, 
+                     refit=True)
+
+gs_et.fit(X_train_tree_raw, y_train.values.ravel())
+
+print("10-Fold CV RMSE:", -gs_et.best_score_)  
+print("Optimal Parameters:", gs_et.best_params_)
+print("Optimal Estimator:", gs_et.best_estimator_)
+
+final_model_et = gs_et.best_estimator_
+
+# Save the trained model for future use (stacking)
+with open("models/final_model_et.pkl", "wb") as f:
+    pickle.dump(final_model_et, f)
+print("ExtraTreesRegressor model saved to models/final_model_et.pkl")
+
+evaluate_model(final_model_et, X_val_tree_raw, y_val, "Extra Trees Regressor")
+
 
