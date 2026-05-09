f6f31c6ab388a045e71ae216a7184614ac3c7738
Dan Dan <ddan@pnmac.com>
2026-05-09 09:49:25 -0700

    s2_model/a3_knn.py: add feature selection for knn

--- s2_model/a3_knn.py
+++ s2_model/a3_knn.py
@@ -20,8 +20,36 @@ cv = KFold(n_splits=10, shuffle=True, random_state=42)
 X_train_knn_raw = load_df(conn, "X_train_knn")
 X_val_knn_raw = load_df(conn, "X_val_knn")
 test_knn_raw = load_df(conn, "test_knn")
-y_train_knn = load_df(conn, "y_train")
-y_val_knn = load_df(conn, "y_val")
+y_train = load_df(conn, "y_train")
+y_val = load_df(conn, "y_val")
+
+
+knn_features = [
+    "log_LotFrontage", "log_LotArea", "OverallQual", "OverallCond", "cbrt_MasVnrArea",
+    "sqrt_BsmtFinSF1",
+    "BsmtFinSF2", "sqrt_BsmtUnfSF", "sqrt_TotalBsmtSF", "log_1stFlrSF", "log_2ndFlrSF", "log_GrLivArea",
+    "BsmtFullBath", "FullBath", "HalfBath", "TotRmsAbvGrd", "Fireplaces", "GarageCars",
+    "GarageArea", "sqrt_WoodDeckSF", "cbrt_OpenPorchSF", "EnclosedPorch", "ScreenPorch", "Age_House",
+    "log_Yrs_Since_Remodel", "log_Age_Garage", "ExterQual_encoded", "BsmtQual_encoded",
+    "BsmtCond_encoded", "BsmtExposure_encoded", "BsmtFinType1_encoded", "KitchenQual_encoded",
+    "FireplaceQu_encoded", "GarageFinish_encoded", "GarageQual_encoded", "GarageCond_encoded",
+    "PoolQC_encoded", "Functional_encoded", "FinishedAreaPct", "Living_Rooms", "Garage_Space",
+    "Garage_AgeCars", "Ratio_Bedroom_Rooms", "Ratio_2ndFlr_Living", "MSSubClass_MSZoning_20_RL",
+    "MSSubClass_MSZoning_50_RL", "MSSubClass_MSZoning_60_RL", "MSSubClass_MSZoning_70_RL",
+    "MSSubClass_MSZoning_120_RL", "MSSubClass_MSZoning_160_RL", "Neighborhood_Condition_CollgCr_Norm",
+    "Neighborhood_Condition_Crawfor_Norm", "Neighborhood_Condition_Gilbert_Norm",
+    "Neighborhood_Condition_NAmes_Norm", "Neighborhood_Condition_NoRidge_Norm",
+    "Neighborhood_Condition_NridgHt_Norm", "Neighborhood_Condition_OldTown_Norm",
+    "Neighborhood_Condition_Somerst_Norm", "Neighborhood_Condition_StoneBr_Norm",
+    "GarageType_Attchd", "GarageType_BuiltIn", "GarageType_Detchd", "CentralAir_Electrical_N_SBrkr",
+    "CentralAir_Electrical_Y_SBrkr", "PavedDrive_N", "PavedDrive_Y", "SaleCondition_Abnorml",
+    "SaleCondition_Normal", "SaleCondition_Partial", "Foundation_CBlock", "Foundation_PConc",
+    "Season_Sold_Spring", "Season_Sold_Summer"
+]
+
+X_train_knn = X_train_knn_raw[knn_features]
+X_val_knn = X_val_knn_raw[knn_features]
+test_knn = test_knn_raw[knn_features]
 
 knn = KNeighborsRegressor()
 
@@ -37,7 +65,7 @@ gs_knn = GridSearchCV(estimator=knn,
                       n_jobs=-1,
                       refit=True)
 
-gs_knn.fit(X_train_knn_raw, y_train_knn.values.ravel())
+gs_knn.fit(X_train_knn, y_train.values.ravel())
 
 print("KNN 10-Fold CV RMSE:", -gs_knn.best_score_)
 print("KNN Optimal Parameter:", gs_knn.best_params_)
@@ -48,10 +76,10 @@ with open("models/final_model_knn.pkl", "wb") as f:
     pickle.dump(best_knn_model, f)
 print("KNN model saved to models/final_model_knn.pkl")
 
-save_df(conn, X_train_knn_raw, "X_train_knn_final")
-save_df(conn, X_val_knn_raw, "X_val_knn_final")
-save_df(conn, test_knn_raw, "test_knn_final")
+save_df(conn, X_train_knn, "X_train_knn_final")
+save_df(conn, X_val_knn, "X_val_knn_final")
+save_df(conn, test_knn, "test_knn_final")
 
-evaluate_model(best_knn_model, X_val_knn_raw, y_val_knn, "KNN Model")
+evaluate_model(best_knn_model, X_val_knn, y_val, "KNN Model")
 
 conn.close()
